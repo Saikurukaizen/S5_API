6ff12b36af3296048a6d26aa31f1bad5552a1886
Marc Sanchez <mks4nch3z@gmail.com>
2025-09-14 19:14:55 +0200

    separating testing logic CRUD vs API

---
+++ S5.01_API_REST/fitbit_api/tests/Feature/Api/DisciplineApiTest.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace Tests\Feature\Api;
+
+use Tests\TestCase;
+use App\Models\User;
+use App\Models\Discipline;
+use Laravel\Passport\Passport;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+//This test checks the API endpoints for the Discipline model.
+// Use the command Passport::actingAs($user) to simulate an authenticated user with different roles
+//with package Passport.
+
+//The test check if an admin user can create, update and delete a discipline
+//and that a regular user or guest cannot perform these actions.
+
+class DisciplineApiTest extends TestCase{
+    
+    use RefreshDatabase;
+
+    /** @test */
+
+    public function admin_can_create_a_discipline(): void{
+
+        $admin = User::factory()->create([
+            'role' => 'admin',
+        ]);
+        Passport::actingAs($admin);
+
+        $response = $this->post('/api/disciplines', [
+            'name' => 'New Discipline',
+            'description' => 'Discipline Description',
+        ]);
+
+        $response->assertStatus(201);
+
+        $this->assertDatabaseHas('disciplines', [
+            'name' => 'Karate',
+            'description' => 'Japanese combat martial art',
+        ]);
+
+        $this->assertCount(1, Discipline::all());
+        
+        $discipline = Discipline::first();
+
+        
+        $this->assertEquals($discipline->name, 'Karate');
+        $this->assertEquals($discipline->description, 'Japanese combat martial art');
+    }
+
+    /** @test */
+    public function user_cannot_create_a_discipline_in_api(): void{
+
+    }
+
+    /** @test */
+    public function guest_cannot_create_a_discipline_in_api(): void{
+
+    }
+
+    /** @test */
+    public function admin_can_update_a_discipline(): void{
+
+    }
+
+    /** @test */
+    public function user_cannot_update_a_discipline(): void{
+
+    }
+
+    /** @test */
+    public function guest_cannot_update_a_discipline(): void{
+
+    }
+
+    /** @test */
+    public function admin_can_delete_a_discipline(): void{
+
+    }
+
+    /** @test */
+    public function user_cannot_delete_a_discipline(): void{
+
+    }
+
+    /** @test */
+    public function guest_cannot_delete_a_discipline(): void{
+
+    }
+}
+?>
--- S5.01_API_REST/fitbit_api/tests/Feature/DisciplineManagementTest.php
+++ S5.01_API_REST/fitbit_api/tests/Feature/DisciplineManagementTest.php
@@ -2,59 +2,45 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
+use App\Models\Discipline;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class DisciplineManagementTest extends TestCase
 {
+    //This managementTest checks the CRUD operations for the Discipline model.
+    use RefreshDatabase;
     /** @test */ 
-    public function a_discipline_can_be_created(){
-        /*
-        Estructura de test: verifica si se puede crear una disciplina (u otra categoria)
-        1. Hace una peticion POST a la ruta /disciplines con datos de ejemplo.
-        2. Verifica que la respuesta sea OK (200).
-        3. Verifica que haya exactamente una disciplina en la base de datos.
-        4. Verifica que los datos de la disciplina creada coincidan con los datos enviados
-        */
+    public function admin_can_create_a_discipline(): void{
 
-        $this->withoutExceptionHandling();
-        $response = $this->post('/disciplines', [
-            'name' => 'Disc Name',
-            'description' => 'Disc Desc.'
+        $admin = User::factory()->create([
+            'role' => 'admin',
+        ]);
+        
+        $discipline = Discipline::create([
+            'name' => 'Karate',
+            'description' => 'Japanese combat martial art',
         ]);
 
-        $this->assertOk();
-        $this->assertCount(1, Discipline::all());
+        $this->assertDatabaseHas('disciplines', [
+            'name' => 'Karate',
+            'description' => 'Japanese combat martial art',
+        ]);
 
-        $discipline = Discipline::first();
-
-        $this->assertEquals($discipline->name, 'Disc Name');
-        $this->assertEquals($discipline->description, 'Disc Desc.');
-        
     }
 
-    public function a_discipline_can_be_read(){
-        /*
-        Estructura de test: verifica si se puede leer una disciplina (u otra categoria)
-        1. Crea una disciplina con una peticion POST a la ruta /disciplines.
-        2. Verifica que haya exactamente una disciplina en la base de datos.
-        3. Obtiene la disciplina creada.
-        4. Verifica que los datos de la disciplina creada coincidan con los datos enviados
-        */
-
-        $this->withoutExceptionHandling();
-        $this->post('/disciplines', [
-            'name' => 'Disc Name',
-            'description' => 'Disc Desc.'
+    public function user_cannot_create_a_discipline(): void{
+        $user = User::factory()->create([
+            'role' => 'user',
         ]);
 
-        $this->assertCount(1, Discipline::all());
-
-        $discipline = Discipline::first();
-
-        $this->assertEquals($discipline->name, 'Disc Name');
-        $this->assertEquals($discipline->description, 'Disc Desc.');
+        $this->expectException(\Illuminate\Auth\AuthenticationException::class);
+        $this->actingAs($user);
+        Discipline::create([
+            'name' => 'Karate',
+            'description' => 'Japanese combat martial art',
+        ]);
     }
 
     public function a_discipline_can_be_updated(){
