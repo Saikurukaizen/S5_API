f50d67d039b82176233e69e1110585e338cd37cd
Marc Sanchez <mks4nch3z@gmail.com>
2025-09-15 10:59:20 +0200

    Defining endpoints idea

---
+++ S5.01_API_REST/fitbit_api/tests/Unit/Policies/DisciplinePolicyTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests\Unit\Policies;
+
+use Tests\TestCase;
+use App\Models\User;
+use App\Models\Community;
+use App\Models\Discipline;
+use App\Policies\DisciplinePolicy;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class DisciplinePolicyTest extends TestCase{
+
+    use RefreshDatabase;
+
+    /** @test */
+    
+}
